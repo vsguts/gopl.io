b67930f32e27a131e8e6a759f3d14bad79945ac7
vsguts <vsguts@gmail.com>
2022-08-11 17:37:19 +0300

    Ch2: Ex 2.1 finished.

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -13,9 +13,9 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
-func CToK(c Celsius) Kelvin { return Kelvin(c + AbsoluteZeroC) }
+func CToK(c Celsius) Kelvin { return Kelvin(c + (-AbsoluteZeroC)) }
 
-func KToC(k Kelvin) Celsius { return Celsius(k) - AbsoluteZeroC }
+func KToC(k Kelvin) Celsius { return Celsius(k) - (-AbsoluteZeroC) }
 
 func FToK(f Fahrenheit) Kelvin { return CToK(FToC(f)) }
 
