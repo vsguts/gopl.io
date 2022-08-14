a4a2d2fa896707d9a18a498db813b3bee67e7db2
vsguts <vsguts@gmail.com>
2022-08-14 12:17:35 +0300

    Restored some comments.

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -1,3 +1,11 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 10.
+//!+
+
+// Dup2 prints the count and text of lines that appear more than once
+// in the input.  It reads from stdin or from a list of named files.
 package main
 
 import (
@@ -45,5 +53,6 @@ func countLines(f *os.File, counts map[string]int) bool {
 			found = true
 		}
 	}
+	// NOTE: ignoring potential errors from input.Err()
 	return found
 }
